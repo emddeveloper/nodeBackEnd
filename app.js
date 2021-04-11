299c7d7bf9cf0114f54a9904eb0577c0eb37c86e
emddeveloper <emd.developer@gmail.com>
2021-04-11 22:47:27 +0530

    ddd

--- app.js
+++ app.js
@@ -5,38 +5,22 @@ const mysql = require("mysql")
 const app = express()
 const port = process.env.PORT || 5000
 
-app.use(bodyParser.urlencoded({ extended: false }))
-
-app.use(bodyParser.json())
-
-// MySQL Code goes here
-const pool = mysql.createPool({
-  connectionLimit: 10,
+var mysql = require("mysql")
+var connection = mysql.createConnection({
   host: "45.87.80.103",
   user: "u769955481_mybalance",
   password: "mybalance",
   database: "u769955481_mybalance"
 })
 
-// Get all beers
-app.get("", (req, res) => {
-  pool.getConnection((err, connection) => {
-    if (err) throw err
-    console.log("connected as id " + connection.threadId)
-    connection.query("SELECT * from mybalance", (err, rows) => {
-      connection.release() // return the connection to pool
+connection.connect()
 
-      if (!err) {
-        res.send(rows)
-      } else {
-        console.log(err)
-      }
+connection.query("SELECT * from mybalance", function (err, rows, fields) {
+  if (err) throw err
 
-      // if(err) throw err
-      console.log("The data from beer table are: \n", rows)
-    })
-  })
+  console.log("The solution is: ", rows[0].solution)
 })
 
+connection.end()
 // Listen on enviroment port or 5000
 app.listen(port, () => console.log(`Listening on port ${port}`))
