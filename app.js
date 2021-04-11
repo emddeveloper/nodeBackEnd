8ff8102c1f176544f3ce5583ae9828cb0fa801a3
emddeveloper <emd.developer@gmail.com>
2021-04-11 22:49:42 +0530

    ddd

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
 const express = require("express")
-const bodyParser = require("body-parser")
-const mysql = require("mysql")
-
 const app = express()
-const port = process.env.PORT || 5000
+const port = 3000
 
-var mysql = require("mysql")
-var connection = mysql.createConnection({
-  host: "45.87.80.103",
-  user: "u769955481_mybalance",
-  password: "mybalance",
-  database: "u769955481_mybalance"
+app.get("/", (req, res) => {
+  res.send("Hello World!")
 })
 
-connection.connect()
-
-connection.query("SELECT * from mybalance", function (err, rows, fields) {
-  if (err) throw err
-
-  console.log("The solution is: ", rows[0].solution)
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 })
-
-connection.end()
-// Listen on enviroment port or 5000
-app.listen(port, () => console.log(`Listening on port ${port}`))
